40e161cc8d83fee0adb558246a1951ff2ff61572
SVNRiS3 <krzy.driwa@gmail.com>
2024-03-21 12:35:55 +0100

    Fix active class to only show on last clicked task/project

--- src/Render.js
+++ src/Render.js
@@ -32,26 +32,29 @@ export default class Render {
 
   addProjectListener(element, item) {
     element.addEventListener('click', () => {
+      document
+        .querySelectorAll(`.${item.type}-active`)
+        .forEach((el) => el.classList.remove(`${item.type}-active`));
       element.classList.add(`${item.type}-active`);
       if (item.type === 'project') this.renderProjectTasks(item);
       else this.renderTaskEdit(item);
     });
   }
 
-  addRemoveElementListener(list, button) {
+  addRemoveElementListener(type, button) {
     const elementId = +button.dataset.id;
     button.addEventListener('click', () => {
       const connectedElements = document.querySelectorAll(
         `[data-id="${elementId}"]`
       );
       connectedElements.forEach((el) => (el.innerHTML = ''));
-      if (list[0].type === 'project')
+      if (type === 'project')
         projects = [
           ...projects.filter(
             (projectItem) => projectItem.getId() !== elementId
           ),
         ];
-      else if (list[0].type === 'task') {
+      else if (type === 'task') {
         projects = projects.map((project) => {
           project.taskList = project.taskList.filter(
             (task) => task.getId() !== elementId
@@ -63,37 +66,29 @@ export default class Render {
   }
 
   renderButtonList(list) {
+    const listType = list[0].type || 'default';
     const listWrapper =
-      document.querySelector(`.${list[0].type || 'default'}-list-wrapper`) ||
-      this.create('div', `${list[0].type || 'default'}-list-wrapper`);
+      document.querySelector(`.${listType}-list-wrapper`) ||
+      this.create('div', `${listType}-list-wrapper`);
     listWrapper.innerHTML = '';
     const addNewButton = this.create(
       'button',
-      `${list[0].type || 'default'}-new`,
-      `Add new ${list[0].type || 'default'}`
+      `${listType}-new`,
+      `Add new ${listType}`
     );
+    // this.addAddElementListener(listType);
     list.forEach((item) => {
       let itemId = item.getId();
-      let elWrapper = this.create(
-        'div',
-        `${list[0].type || 'default'}-el-wrapper`,
-        '',
-        itemId
-      );
-      let button = this.create(
-        'button',
-        list[0].type || 'default',
-        item.title,
-        itemId
-      );
+      let elWrapper = this.create('div', `${listType}-el-wrapper`, '', itemId);
+      let button = this.create('button', listType, item.title, itemId);
       this.addProjectListener(button, item);
       let removeItemEl = this.create(
         'button',
-        `${list[0].type || 'default'}-remove`,
+        `${listType}-remove`,
         'X',
         itemId
       );
-      this.addRemoveElementListener(list, removeItemEl);
+      this.addRemoveElementListener(listType, removeItemEl);
       elWrapper.appendChild(button);
       elWrapper.appendChild(removeItemEl);
       listWrapper.appendChild(elWrapper);
